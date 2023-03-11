7821c35fc6affefa68659796a3f4f3119cf68ffd
joaolevi <joaolevi777@gmail.com>
2023-03-11 09:59:56 -0300

    Adding Tests

--- internal/entity/order_test.go
+++ internal/entity/order_test.go
@@ -10,3 +10,22 @@ func TestIfIGetAnErrorIfIDIsBlank(t *testing.T) {
 	order := Order{}
 	assert.Error(t, order.Validate(), "invalid id")
 }
+
+func TestIfIGetAnErrorIfPriceIsBlank(t *testing.T) {
+	order := Order{ID: "123"}
+	assert.Error(t, order.Validate(), "invalid price")
+}
+
+func TestIfIGetAnErrorIfTaxIsBlank(t *testing.T) {
+	order := Order{ID: "123", Price: 10.0}
+	assert.Error(t, order.Validate(), "invalid tax")
+}
+
+func Test_WithAllValidParams(t *testing.T) {
+	order := Order{ID: "123", Price: 10.0, Tax: 1.0}
+	assert.NoError(t, order.Validate())
+	assert.Equal(t, 10.0, order.Price)
+	assert.Equal(t, 1.0, order.Tax)
+	order.CalculateFinalPrice()
+	assert.Equal(t, 11.0, order.FinalPrice)
+}
